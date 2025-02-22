dc9d50b5818c4d072eeb6c1c49ebb9ce660ae043
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-02-22 13:29:42 +0300

    Create Day2.cpp
    learned how to use comments which includes single and multiple line. Also \n helps to start on new line when using output.

---
+++ Day2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  cout<<"Day 2 of coding cpp. Using single and multiple line comments\n"; //This is single line comment.
+  cout<<"Notice! we need semicolon at end of every statement or line to indicate the statement ended.\n";
+  cout<<"Also, forward slash n helps to break line and continue on new line";
+/*This
+  is
+  multiple
+  line
+  comment. */
+}
