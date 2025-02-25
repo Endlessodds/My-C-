fadf8bf50b589a9153d61efee76c2e3e2fdb4b2a
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-02-25 16:55:25 +0300

    Create Day3.cpp
    using cin

---
+++ Day3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string firstname;
+    // declaring a variable for cin.
+    cin >> firstname;
+    // storing input from user and holding place in variable named "firstname".
+    cout << "hello " << firstname << " welcome";  // Added spaces for proper formatting
+    return 0;
+}
