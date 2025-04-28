b4a113f9b639270c015de61c36789a925fc4c8b2
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-04-28 21:58:42 +0300

    Create Project1.cpp

---
+++ Project1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string q1, q2, q3;
+
+    cout << "What's your favorite food? ";
+    cin >> q1;
+
+    cout << "What's your favorite color? ";
+    cin >> q2;
+
+    cout << "What's your dream city? ";
+    cin >> q3;
+
+    cout << "\nThanks for your answers!\n";
+    cout << "Food: " << q1 << ", Color: " << q2 << ", City: " << q3 << "\n";
+
+    return 0;
+
