d3581ecbec6a48136ad7494eb277bc0c7550c8b9
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-04-29 08:06:30 +0300

    Update Day9.cpp for future project

--- Day9.cpp
+++ Day9.cpp
@@ -9,6 +9,7 @@ int main() {
 
     cout << "Character: " << userChar << "\n";
     cout << "ASCII Code: " << (int)userChar << "\n";
-
+//Next project: using ASCII code, develop encryption and decryption
+// that accepts inputs for encryption and outputs a decryption.
     return 0;
 }
