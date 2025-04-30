359e3848cc0d7ff4713ffb4c717c3d006ab7a597
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-04-30 15:57:39 +0300

    Create Day10.cpp

---
+++ Day10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 10, b = 3;
+  a + b;  // 13
+  a - b;  // 7
+  a * b;  // 30
+  a / b;  // 3 (integer division)
+  a % b;  // 1 (modulo)
+
+  return 0;
+}
