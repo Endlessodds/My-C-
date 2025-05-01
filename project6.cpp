f7b13cde2e929a4a5363ee8047e895dc1bc51299
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-01 14:31:37 +0300

    Create project6.cpp

---
+++ project6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int dividend = 29;
+    int divisor = 5;
+
+    int remainder = dividend % divisor;
+
+    cout << "Remainder: " << remainder << endl;
+
+    return 0;
+}
