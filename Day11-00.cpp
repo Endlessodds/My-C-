7ded6e7bca57d678f6187e2f9f83bde01af79790
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-02 12:40:16 +0300

    Create Day11-00 datatypes for integer types

---
+++ Day11-00.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a = 5;       // 4 bytes, stores integer
+short b = 10;    // 2 bytes, stores smaller integer
+long c = 1000;   // 4 bytes, stores larger integer
+long long d = 100000;  // 8 bytes, stores even larger integer
+
+
+ return 0;
+}
