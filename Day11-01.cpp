e2af69378637a7d5327b055a5cb577ac076fc723
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-02 12:44:08 +0300

    Create Day11-01 datatype float types

---
+++ Day11-01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+float x = 5.75f;      // 4 bytes, single precision
+double y = 3.14159;   // 8 bytes, double precision
+long double z = 2.71828L;  // Extended precision
+
+return 0;
+
+}
