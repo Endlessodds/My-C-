c306a0c1e16d62d9e82809349a60b6d029a94bd5
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-02 12:50:24 +0300

    Create Day11-03 datatypes bool type

---
+++ Day11-03.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   bool flag = true;   // 1 byte, stores true or false
+    cout<<"The output is 1 bcs true holds a value of 1"<<flag;
+    return 0;
+}
