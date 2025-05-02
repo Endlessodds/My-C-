bef68f00bdc7242170f8ab28b79c8dc38b6dcc2d
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-02 12:54:26 +0300

    Create Project4 combines concepts from day 11 wc is about datatypes

---
+++ Project4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Integer Types
+    int a = 10;
+    short b = 5;
+    long c = 10000;
+    long long d = 1000000;
+    
+    // Floating Point Types
+    float x = 3.14f;
+    double y = 3.14159265359;
+    long double z = 2.718281828459045;
+    
+    // Character Type
+    char letter = 'A';
+    
+    // Boolean Type
+    bool isTrue = true;
+    
+    // Displaying values
+    cout << "a (int): " << a << endl;
+    cout << "b (short): " << b << endl;
+    cout << "c (long): " << c << endl;
+    cout << "d (long long): " << d << endl;
+    cout << "x (float): " << x << endl;
+    cout << "y (double): " << y << endl;
+    cout << "z (long double): " << z << endl;
+    cout << "letter (char): " << letter << endl;
+    cout << "isTrue (bool): " << isTrue << endl;
+    
+    return 0;
+}
