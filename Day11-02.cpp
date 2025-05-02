6d29881ac62e98b259144e7ad1f99693bb4ea0c8
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-02 12:47:28 +0300

    Create Day11-02 datatypes character type

---
+++ Day11-02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  char letter = 'A';  // 1 byte, stores a single character
+  cout<<"The output is single character. as you can see-- "letter;
+    return 0;
+}
