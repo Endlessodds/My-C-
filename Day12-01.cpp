bd3bf6d3c3e79327535d3e0a617c898efa96fcf7
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-03 21:11:31 +0300

    Create Day12-01 Logical operators

---
+++ Day12-01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1, b = 0;
+    cout << "a && b: " << (a && b) << endl;
+    cout << "a || b: " << (a || b) << endl;
+    cout << "!a: " << (!a) << endl;
+    return 0;
+}
