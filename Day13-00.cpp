df427e896c7bde12becbee02fa83081321c1412e
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-04 12:51:48 +0300

    Create Day13-00 if else

---
+++ Day13-00.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int age = 16;
+if (age >= 18) {
+    std::cout << "You are an adult.\n";
+} else {
+    std::cout << "You are a minor.\n";
+}
+
+return 0;
+}
