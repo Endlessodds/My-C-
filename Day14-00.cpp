f39ea4bf2bf793620948fd7db1c33ab11ddd8634
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-06 19:58:08 +0300

    Day14-00 while loop

---
+++ Day14-00.cpp
@@ -0,0 +1,13 @@
+// while (condition) {
+    // Code to execute while condition is true
+// }
+#include <iostream>
+using namespace std;
+int main(){
+int i = 0;
+while (i < 5) {
+    cout << i << endl;
+    i++;
+}
+return 0;
+}
