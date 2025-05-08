13015c3c0739fb878d26ddbcfc3ba0bc7854875f
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-08 16:24:46 +0300

    Project38 Simple Menu Display

---
+++ Project38.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void showMenu() {
+    cout << "=== Menu ===" << endl;
+    cout << "1. Add" << endl;
+    cout << "2. Subtract" << endl;
+    cout << "3. Exit" << endl;
+}
+
+int main() {
+    showMenu();
+    return 0;
+}
