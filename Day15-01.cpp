ad8a3050fd1be25c0be70867861a171ccfff271d
Aben <156015109+Endlessodds@users.noreply.github.com>
2025-05-08 15:57:31 +0300

    Day15-01 Void Functions

---
+++ Day15-01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void greetUser() {
+    cout << "Welcome to the C++ Function Practice!\n Void returns only output/prints. No calculating for void;)" << endl;
+}
+
+int main() {
+    greetUser();
+    return 0;
+}
