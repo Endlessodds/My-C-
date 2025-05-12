bee848b64be1e43807adbea8aa29cb27807ce88c
Abenezer K. <156015109+Endlessodds@users.noreply.github.com>
2025-05-12 18:24:13 +0300

    Ever evolving calculator

---
+++ Calculator.cpp
@@ -0,0 +1,21 @@
+// An Evolving project (changes are based on better c++ concepts)
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+
+    // Input two numbers
+    cin >> num1 >> num2;
+
+    // Output the sum
+    cout << "The sum is "<<num1 + num2 << endl;
+    cout << "The difference is "<<num1 - num2 << endl;
+    cout << "The product is "<<num1 * num2 << endl;
+    cout << "The quotient is "<<num1 / num2 << endl;
+
+    return 0;
+}
+
+}
+ 
