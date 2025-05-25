38ee93f1d2719587d8132bcf39e5c37d0fbd5270
Abenezer K. <156015109+Endlessodds@users.noreply.github.com>
2025-05-25 22:06:10 +0300

    Update Calculator v0.2
    Menu added
    Errors handled well (on zero division and operation selection)
    User able to select Operation

--- Ever-evolving/Calculator/Calculator.cpp
+++ Ever-evolving/Calculator/Calculator.cpp
@@ -16,26 +16,62 @@ double multiply(double a, double b) {
 }
 
 double divide(double a, double b) {
-    // Handle division by zero
     if (b == 0) {
+        // Handle division by zero
         cout << "Error: Division by zero." << endl;
-        return 0;
+        return 1;
     }
     return a / b;
 }
 
+void menu() {
+    cout << "Welcome ======\n";
+    cout << "Choose one:\n";
+    cout << "  + : Addition\n";
+    cout << "  - : Subtraction\n";
+    cout << "  * : Multiplication\n";
+    cout << "  / : Division\n";
+    cout << "  a : All operations\n";
+}
+
 int main() {
     double num1, num2;
+    char operation;
+
+    menu(); // Show menu
 
     // Input two numbers
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
 
-    // Output results using functions
-    cout << "The sum is " << add(num1, num2) << endl;
-    cout << "The difference is " << subtract(num1, num2) << endl;
-    cout << "The product is " << multiply(num1, num2) << endl;
-    cout << "The quotient is " << divide(num1, num2) << endl;
+    // Input operation choice
+    cout << "Enter operation (+, -, *, /, a): ";
+    cin >> operation;
+    // Matching operation with signs to call Function
+    if (operation == '+') {
+        cout << "Result: " << add(num1, num2) << endl;
+    }
+    else if (operation == '-') {
+        cout << "Result: " << subtract(num1, num2) << endl;
+    }
+    else if (operation == '*') {
+        cout << "Result: " << multiply(num1, num2) << endl;
+    }
+    else if (operation == '/') {
+        cout << "Result: " << divide(num1, num2) << endl;
+    }
+    else if (operation == 'a') {
+      // outputs all operations
+        cout << "Sum: " << add(num1, num2) << endl;
+        cout << "Difference: " << subtract(num1, num2) << endl;
+        cout << "Product: " << multiply(num1, num2) << endl;
+        cout << "Quotient: " << divide(num1, num2) << endl;
+    }
+    else {
+      // handles error correctly
+        cout << "Invalid operation entered." << endl;
+        return 1;
+    }
 
     return 0;
 }
